1b78a18f4e666f1135db1a6394b21abfe1bb8f2c
khadiza21 <bibikhadiza474@gmail.com>
2022-02-21 01:58:40 +0600

    six core concept of javascript

---
+++ intermediateJS/sixCoreConceptJs.js
@@ -0,0 +1,39 @@
+// intermediateJS/sixCoreConceptJs.js
+
+//1. variable
+var deposite = 2332;
+
+//2.  conditions
+if (deposite < 300 && deposite != 3434) {
+}
+
+//3. array
+const numbers = [21, 23, 232, 434, 44];
+console.log(numbers.length);
+console.log(numbers.pop());
+console.log(numbers.push(111));
+console.log((numbers[2] = 555));
+console.log();
+
+// check weather 222 included in the array
+if (numbers.indexOf(222) != -1) {
+}
+
+if (numbers.indexOf(222)) {
+  console(true);
+}
+
+// 4. loop
+//while, for
+for (const number of numbers) {
+}
+
+//5. function
+function fullname() {
+  const name = first + " " + localStorage;
+}
+
+const person = fullname("Abdur", "Rahaman");
+
+//6,  object
+const bottle = { color: "pink", contain: "water", price: 30 };
--- intermediateJS/test.js
+++
@@ -1,9 +0,0 @@
-// intermediateJS
-const players = [
-  { name: { firstName: "Shakib", lastName: "Hasan" }, age: 34 },
-  { name: { firstName: "Tamim", lastName: "Iqbal" }, age: 32 },
-  { name: { firstName: "Mahmudullah", lastName: "Riyad" }, age: 36 },
-  { name: { firstName: "Mashrafe", lastName: "Murtaza" }, age: 38 },
-];
-
-console.log(players.map((player) =>player.name.firstName));
