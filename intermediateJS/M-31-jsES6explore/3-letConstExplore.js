4310d3060aff2f465cf2165eb034cf59cdc0379d
khadiza21 <bibikhadiza474@gmail.com>
2022-02-21 18:53:50 +0600

    let and const keyword of js explore

---
+++ intermediateJS/M-31-jsES6explore/3-letConstExplore.js
@@ -0,0 +1,19 @@
+// variable
+//let
+let num1 = 12;
+num1 = 23;
+
+//const
+const num2 = 34;
+// num2 =345; //not allowed
+
+// array
+const numbers = [34, 34, 23, 45, 36, 23];
+//numbers = [34,65,75,23] //not allowed full const array chancge
+numbers.push(555); //allowed
+numbers[1] = 233; //allowed
+
+//object
+const student = { id: 121, name: "Tanha", job: "intern" };
+student.name = "Khadiza";
+student = { name: "madumami" }; //const not allowed reassign
