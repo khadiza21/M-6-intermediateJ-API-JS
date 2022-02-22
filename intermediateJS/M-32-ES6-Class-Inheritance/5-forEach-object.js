2cb05e65650a88c8279c6e3268305499fbc6e848
khadiza21 <bibikhadiza474@gmail.com>
2022-02-23 02:52:23 +0600

    array for each loop  with object propertyy

---
+++ intermediateJS/M-32-ES6-Class-Inheritance/5-forEach-object.js
@@ -0,0 +1,11 @@
+const products = [
+  { productName: "Mobile", price: 24000, color: "black", quantity: 2 },
+  { productName: "watch", price: 4000, color: "black", quantity: 4 },
+  { productName: "water pot", price: 240, color: "pink", quantity: 6 },
+  { productName: "books", price: 7000, color: "customized", quantity: 120 },
+  { productName: "chips", price: 100, color: "red", quantity: 30 },
+  { productName: "bags", price: 260, color: "black", quantity: 12 },
+];
+
+// products.map((product) => console.log(product));
+products.forEach((product) => console.log(product));
