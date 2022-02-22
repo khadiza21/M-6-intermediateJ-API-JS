d42ff65986ee3a79c99e35c55178e1101239b283
khadiza21 <bibikhadiza474@gmail.com>
2022-02-22 12:52:51 +0600

    template string

---
+++ intermediateJS/M-31-jsES6explore/5-templateString.js
@@ -0,0 +1,19 @@
+const priya = "Asif Akbar";
+const line1 = `lorem kokjd jsast jd eu `;
+const multiline = `
+sfdf dfkjfs dfjsdlds
+sdfjdfsdf dfuiofdj 
+sfjoe skdjfoei se ifjdsld djforj e
+dfdjf eodjfdio`;
+
+const firstName = "Bibi";
+const lastName = "Khadiza";
+console.log(`My name is ${firstName} ${lastName}`);
+
+const num = 12;
+const old = ` <h3>Friend- ${num}</h3>`;
+console.log(old);
+
+const friends = ["tanha", "bk", "khadiza", "tania", "tani", "tanu"];
+const myFriends = ` <h3>Friends numbers are ${friends.length}</h3>`;
+console.log(myFriends);
