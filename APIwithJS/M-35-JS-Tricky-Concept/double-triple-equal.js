2b69e1320f11332205188baf1cfc10f89dd7b174
khadiza21 <bibikhadiza474@gmail.com>
2022-02-28 03:14:45 +0600

    double and triple equal

---
+++ APIwithJS/M-35-JS-Tricky-Concept/double-triple-equal.js
@@ -0,0 +1,21 @@
+//const first = "1";
+//const first = 1;
+const first = 2;
+
+const second = true;
+if (first == second) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+
+// more comparison 
+const a = {name: 'ali'};
+const b = {name: 'ali'};
+if (a == b){
+    console.log('both are same');
+}
+else{
+    console.log('they are not same');
+}
