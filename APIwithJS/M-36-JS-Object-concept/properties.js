6462978b4a36c782ddb10765c734becd22d839bb
khadiza21 <bibikhadiza474@gmail.com>
2022-02-28 06:57:31 +0600

    explore key,vlaue,entires,delete,seal,freeze keyword  applied on js objectt

---
+++ APIwithJS/M-36-JS-Object-concept/properties.js
@@ -0,0 +1,25 @@
+const bottle = {
+  color: "yellow",
+  hold: "water",
+  price: 50,
+  isCleaned: true,
+};
+// getting all properties names
+const keys = Object.keys(bottle);
+console.log(keys);
+
+// get all values
+const values = Object.values(bottle);
+console.log(values);
+
+//get all entries
+const pairs = Object.entries(bottle);
+console.log(pairs);
+
+// Object.seal(bottle);
+Object.freeze(bottle);
+bottle.price = 100;
+bottle.height = 16;
+//delte object property
+delete bottle.isCleaned;
+console.log(bottle);
